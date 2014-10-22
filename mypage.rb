bb0cf51efcc5edca76951a81370385c6893c04a6
Marcy <marcy9114@gmail.com>
2014-10-22 16:01:30 +0900

    change handler

--- mypage.rb
+++ mypage.rb
@@ -13,13 +13,6 @@ end
 
 login = req.key?("login") ? req[:login] : nil
 
-if login.nil? then
-  hout = Nginx::Headers_out.new
-  cookie = "notice=You must be logged in;"
-  hout["Set-Cookie"] = cookie
-  Nginx.redirect "/", Nginx::HTTP_MOVED_TEMPORARILY
-end
-
 last_login = redis.exists?("last_login_#{login}") ? {created_at: redis.hget("last_login_#{login}", "created_at"), ip: redis.hget("last_login_#{login}", "ip")} : {}
 
 created_at = last_login[:created_at]
---
+++ mypage_rewrite.rb
@@ -0,0 +1,20 @@
+r = Nginx::Request.new
+
+req = {}
+cookie_str  = r.headers_in['Cookie']
+unless cookie_str.nil? then
+  cookie_list = cookie_str.split("; ")
+  cookie_list.each do |cookie|
+    key, val = cookie.split("=")
+    req[key] = val
+  end
+end
+
+login = req.key?("login") ? req[:login] : nil
+
+if login.nil? then
+  hout = Nginx::Headers_out.new
+  cookie = "notice=You must be logged in;"
+  hout["Set-Cookie"] = cookie
+  Nginx.redirect "/", Nginx::HTTP_MOVED_TEMPORARILY
+end
