a1315bcb57659aaa3bf714ee839efff94435d9a7
Marcy <marcy9114@gmail.com>
2014-10-22 18:11:48 +0900

    invalid hash key

--- init.rb
+++ init.rb
@@ -12,22 +12,22 @@ Redis.current.flushall
 now_login = {}
 users     = {}
 
-db.query('SELECT login, ip, succeeded, created_at FROM login_log').each do |login, ip, succeeded, created_at|
+db.query('SELECT login, ip, succeeded, created_at FROM login_log').each do |log|
   if succeeded == 1 then
-    Redis.current.del("ip_fail_#{ip}")
-    Redis.current.del("user_fail_#{login}")
-    Redis.current.hmset("last_login_#{login}", now_login[login].to_a.flatten) if now_login.key?(login)
-    now_login[login] = {created_at: created_at, ip: ip}
+    Redis.current.del("ip_fail_#{log['ip']}")
+    Redis.current.del("user_fail_#{log['login']}")
+    Redis.current.mapped_hmset("last_login_#{log['login']}", now_login[log['login']]) if now_login.key?(log['login'])
+    now_login[login] = {created_at: log['created_at'], ip: log['ip']}
   else
-    Redis.current.incr("ip_fail_#{ip}")
-    Redis.current.incr("user_fail_#{login}")
+    Redis.current.incr("ip_fail_#{log['ip']}")
+    Redis.current.incr("user_fail_#{log['login']}")
   end
 end
 
 db.query('SELECT login, password_hash, salt FROM users').each_slice(10000) do |users|
   fork do
     users.each do |user|
-      Redis.current.hmset("user_#{user[:login]}", user.to_a.flatten)
+      Redis.current.mapped_hmset("user_#{user['login']}", user)
     end
   end
 end
